70bf1be28e12d001ea451565e4c0bd947b45bcda
777bareman777 <45147152+777bareman777@users.noreply.github.com>
2020-09-09 17:09:03 +0900

    Update install-vim-mac.sh

--- install-vim-mac.sh
+++ install-vim-mac.sh
@@ -4,7 +4,7 @@
 brew update
 brew install curl
 brew install global
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/777bareman777/my-vim-setting
 
 cp my-vim-setting/.vimrc-mac ~/.vimrc
