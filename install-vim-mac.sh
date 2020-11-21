5ac3a2ffe33181691102f8f9f0e23c1e24f81d5b
777bareman777 <45147152+777bareman777@users.noreply.github.com>
2020-11-22 01:57:33 +0900

    Update install-vim-mac.sh

--- install-vim-mac.sh
+++ install-vim-mac.sh
@@ -8,5 +8,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/777bareman777/my-vim-setting
 
-cp my-vim-setting/.vimrc-mac ~/.vimrc
+cp my-vim-setting/vimrc ~/.vimrc
 rm -rf my-vim-setting
